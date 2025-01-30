89efc31c7a30a9eaf4969582d990bcaaeb4666c0
Ekaterina Chebotaeva <ek.chebotaeva@yandex.ru>
2025-01-31 01:09:28 +0500

    add test LoadPageTheAIPowered

---
+++ src/test/java/dragAndDrop/dragAndDropElement.java
@@ -0,0 +1,4 @@
+package dragAndDrop;
+
+public class dragAndDropElement {
+}
---
+++ src/test/java/github/LoadPageTheAIPowered.java
@@ -0,0 +1,27 @@
+package github;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class LoadPageTheAIPowered {
+
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void findPageThePowered(){
+        open("https://github.com/");
+        $(".HeaderMenu-nav").$(byText("Solutions")).hover();
+        $(".HeaderMenu-nav").$(byTagAndText("a","Enterprises")).click();
+        $("#hero-section-brand-heading").shouldBe(visible).shouldHave(text("The AI-powered"));
+        sleep(5000);
+    }
+}
