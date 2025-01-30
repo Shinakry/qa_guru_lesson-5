4dc81c20ef5f5da7d0a3c4bea551f0ada4772914
Ekaterina Chebotaeva <ek.chebotaeva@yandex.ru>
2025-01-31 01:28:33 +0500

    add test dragAndDropElement

--- src/test/java/dragAndDrop/dragAndDropElement.java
+++ src/test/java/dragAndDrop/dragAndDropElement.java
@@ -1,4 +1,44 @@
-package dragAndDrop;
+package github;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.Condition.*;
+import static com.codeborne.selenide.Selectors.*;
+import static com.codeborne.selenide.Selenide.*;
 
 public class dragAndDropElement {
+
+    @BeforeAll
+    static void beforeAll(){
+        Configuration.browserSize = "1920x1080";
+    }
+
+    @Test
+    void testDragAndDropWithClickAndHold(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("b"));
+        actions().clickAndHold($("#column-a")).moveToElement($("#column-b")).release().perform();
+        $("#column-a").shouldHave(text("b"));
+        $("#column-b").shouldHave(text("a"));
+    }
+
+    @Test
+    void testDragAndDropWithDragAndDrop(){
+        open("https://the-internet.herokuapp.com/drag_and_drop");
+        $("#column-a").shouldHave(text("A"));
+        $("#column-b").shouldHave(text("b"));
+        $("#column-a").dragAndDrop(DragAndDropOptions.to($("#column-b")));
+        $("#column-a").shouldHave(text("b"));
+        $("#column-b").shouldHave(text("a"));
+    }
+
+    @AfterEach
+    void afterEach(){
+        closeWebDriver();
+    }
 }
